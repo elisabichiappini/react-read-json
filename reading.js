3e12137be78bfcccdf0feb4a422c688b40a506a4
elisabichiappini <elisabichiappini@gmail.com>
2025-01-21 15:55:47 +0100

    feat: lettura file json

--- reading.js
+++ reading.js
@@ -1,28 +1,39 @@
-// const dotenv = require("dotenv").config();
-
-// const server = require("http");
-// const port = process.env.PORT || 1234;
-// const host = process.env.HOST || "localhost";
-// server.createServer((req, res,) => {
-//     res.writeHead(200,
-//         {
-//             "Content-type":"text/html"
-//         }
-//     );
-//     res.end("<h1>ciao sono collegato</h1>");
-// }).listen(port, host, () => {
-//     console.log(`Server avviato su: http://${host}:${port}`);
-// });
+const dotenv = require("dotenv").config();
 
 const fs = require("fs");
 const path = require("path");
 
-const loadData = (nomeFile) => {
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+const readJSONData = (nomeFile) => {
     const filePath = path.join(__dirname, nomeFile + '.json');
     const fileData = fs.readFileSync(filePath, "utf-8");
-    
     return JSON.parse(fileData);
 }
 
-const users = loadData('users');
-console.log(users); //[]
+const writeJSONData = (nomeFile, newData) => {
+    const filePath = path.join(__dirname, nomeFile + '.json');
+    const fileString = JSON.stringify(newData);
+    fs.writeFileSync(filePath, fileString);
+}
+const server = require("http");
+server.createServer((req, res,) => {
+    res.writeHead(200,
+        {
+            "Content-type": "text/html; charset=utf-8"
+        }
+    );
+    let fileHtml;
+    switch(req.url) {
+        case '/':
+            const users = readJSONData('users');
+            fileHtml = '<ul>' 
+            users.forEach(u => fileHtml += `<li>${u.name}</li>`);
+            fileHtml +='</ul>';
+        break;
+    }
+    res.end(fileHtml)
+}).listen(port, host, () => {
+    console.log(`Server avviato su: http://${host}:${port}`);
+});
